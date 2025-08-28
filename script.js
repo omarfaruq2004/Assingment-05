caf7f64494ff03c6f7ba7750cec92de6c107fdc7
unknown <omarfaruq669224@gmail.com>
2025-08-28 20:35:15 +0600

    heart icon feature

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ buttons.forEach(button => {
     })
 })
 
+            //Call Buttons
 
 
 
+// document.getElementById('national-call-btn').addEventListener('click', function(){
+     
+//   const money =parseInt(document.getElementById('money').innerText);
+
+//        if(money < 20){
+//         alert('You do not have enough coin, you need at least 20 coin to make a call ')
+//         return;
+//      }
+
+//     alert(' Calling National Emaergency service 999');
+     
+//      const currentMoney = money - 20;
+
+//     document.getElementById('money').innerText = currentMoney;
+
+// });
+
+
+const callButton =document.querySelectorAll('.call-btn');
+
+callButton.forEach(callBtn => {
+  
+})
