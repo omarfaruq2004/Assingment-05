a687a0e56a1c4e396052ececda79d40d63da0acf
unknown <omarfaruq669224@gmail.com>
2025-08-28 12:55:53 +0600

    heart icon feature compled

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('omar faruq')
+           // Heart icon
+ 
+ const buttons = document.querySelectorAll('.heart-btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        
+    
+  const hearts =parseInt(document.getElementById('heart').innerText);
+ 
+  
+     const current = hearts + 1;
+
+    document.getElementById('heart').innerText = current;
+   
+
+    })
+})
+
+
+
+
